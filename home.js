f9c7ed9d500d523495c2f064c4f53fdf2a0753d8
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-03-04 14:49:33 -0600

    Starting PluralSight JavaScript pt 5

--- home.js
+++ home.js
@@ -1,6 +1,18 @@
 let welcome = 'Welcome to Jackass!'
 
+/*
+let name = 'Hiking boots';
+let price = 49.99;
+let discounted = false;
+*/
+
+// Declaring variables in a group. Is this JS' version of defining a class?
+let price = 49.99,
+    name = 'Hiking boots',
+    discounted = false;
+
 showMessage("Ch-ch-ch-changes...");
 showMessage(welcome);
+showMessage(discounted);
 
 console.log("This aggression will not stand, man.");
