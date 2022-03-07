9275139c1889cbc7db636374a969a8fd69ce293c
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-03-07 16:34:36 -0600

    Looping with while()

--- home.js
+++ home.js
@@ -1,16 +1,17 @@
 // PluralSight JS 6: Program Flow
-// Looping with for()
-
-for (let i = 0; i < 3; i++) {
-    console.log(i);
-}
-// 0 1 2
-// Set i at 0. i will iterate thru 0,1,2. Increment i by 1 on each pass. Print i to console log on each pass.
+// Looping with while()
+// While loops can be easier to handle than for() loops
 
 /*
-for (let i = 0; i < 3; i--) {
-    console.log(i);
+let count = 1;
+while (count < 5) {
+    console.log(count);
+    count++;
 }
-// This leads to an infinite loop. Console continues to print -i numbers endlessly.
-// Shut down the server and browser to stop the cycle.
+
+let i = 4;
+while (i > 0) {
+    console.log(i);
+    i--;
+}
 */
