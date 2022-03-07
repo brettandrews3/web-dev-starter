de4db526e23105393469528072c39d28c1278640
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-03-07 16:48:03 -0600

    Part 6 Summary Notes.

--- home.js
+++ home.js
@@ -1,23 +1,23 @@
 // PluralSight JS 6: Program Flow
-// Looping with do ... while()
-
-// Use do ... while() when the code is guaranteed to run at least once.
+// Summary notes
 
 /*
-let count = 1;
-do {
-    console.log(count);
-    count++;
-} while (count < 5);
-// 1 2 3 4
-// It's guaranteed to run at least once because count starts at 1. It will repeat while 
-// the iterator is less than 5.
-*/
+Review if...else statements
 
-let i = 4;
-do {
-    console.log(i);
-    i--;
-} while (i > 0);
-// Again, it's guaranteed to execute at least once because 'do' will print iterator i to
-// the console BEFORE it checks while(). The while() condition is never met, so it doesn't loop.
+Truthy and falsy expressions:
+- '0' is true, but 0 is false
+- EVERY number is true, except 0
+
+Comparing === to ==
+- Always use === and !== as a best practice
+- JavaScript might try to convert types if you use == and !=
+
+The ternary operator
+- (condition) ? (true-statement) : (false-statement);
+
+Block Scope
+- Variables declared with let|const are block scoped and can't appear outside of it
+
+Loops: for, while, do ... while()
+- Be sure that the loops can complete at some point. Else, browser and server get in infinite loop
+*/
