d58f225a07caf4ccc90d2b1635b81aeb9c5cda1b
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-03-07 16:41:56 -0600

    Looping with do...while()

--- home.js
+++ home.js
@@ -1,17 +1,23 @@
 // PluralSight JS 6: Program Flow
-// Looping with while()
-// While loops can be easier to handle than for() loops
+// Looping with do ... while()
+
+// Use do ... while() when the code is guaranteed to run at least once.
 
 /*
 let count = 1;
-while (count < 5) {
+do {
     console.log(count);
     count++;
-}
+} while (count < 5);
+// 1 2 3 4
+// It's guaranteed to run at least once because count starts at 1. It will repeat while 
+// the iterator is less than 5.
+*/
 
 let i = 4;
-while (i > 0) {
+do {
     console.log(i);
     i--;
-}
-*/
+} while (i > 0);
+// Again, it's guaranteed to execute at least once because 'do' will print iterator i to
+// the console BEFORE it checks while(). The while() condition is never met, so it doesn't loop.
