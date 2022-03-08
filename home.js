4917e0dd4aeacafa6ab229603a15a1cd72506b71
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-03-08 15:44:19 -0600

    Lesson 7.4 - Passing Information to Functions

--- home.js
+++ home.js
@@ -1,12 +1,30 @@
 // PluralSight JS 7: Functions
 // Function Expressions
-
-function showMessage() {
-    
+/*
+// Add message here. The block of code in {} will be stored in message.
+function showMessage(message) {
+    console.log(message);
 }
 
-let myFunction = function () {
-    console.log('Hey! This is a private residence, man!');
+// Here, whatever we enter in () will be passed to console because
+// we're passing 'message' through to the console.log().
+showMessage("I've got information, man!");
+showMessage("What in god's name are you blathering about?");
+*/
+
+/*
+// We can also pass multiple types and values in the same function:
+function showMessages(message, anotherMessage) {
+    console.log(message, anotherMessage);
 }
 
-myFunction();    // calls the function
+showMessages('She probably kidnapped herself.', "Obviously, you're not a golfer.");
+*/
+
+let myFunction = function (message, firstName) {
+    console.log(message);
+    console.log(firstName);
+}
+myFunction('Ever thus to deadbeats,', 'Lebowski.');
+// Any unsupplied parameters will be set to 'undefined'
+// Always make sure that you're passing correct values to functions
