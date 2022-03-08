2a2d40f0b9935f4ba14cc90146c7677f62010ba5
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-03-08 16:27:05 -0600

    Lesson 7.7 - Using Functions to Modify Web Pages

--- home.js
+++ home.js
@@ -1,47 +1,5 @@
 // PluralSight JS 7: Functions
-// Function Scope
+// Using Functions to Modify Web Pages
 
-// Once function completes execution, any variables disappear
-
-/*
-function getSecretCode(value) {
-    let code = value * 42;
-    return code;
-}
-
-let secretCode = getSecretCode(2);
-console.log( code );    // Uncaught ReferenceError: 'code' is not defined
-// Code is undefined because we're not calling the function getSecretCode()
-*/
-
-/*
-// Functions can use variables declared outside the function, so long as the variables come first:
-let key = 42;
-
-function getSecretCode(value) {
-    let code = value * key
-    return code;
-}
-
-let secretCode = getSecretCode(2);
-showMessage( secretCode );
-*/
-
-// Functions can declare variables within their blocks that share global var name:
-let key = 42;
-
-function getSecretCode(value) {
-
-    let keyGenerator = function() {
-        let key = 12;
-        console.log('in keyGenerator: ', key);
-        return key;
-    }
-
-    let code = value * keyGenerator();
-    console.log(' in getSecretCode: ', key);
-    return code;
-}
-
-let secretCode = getSecretCode(2);
-showMessage( secretCode );   // 24
+// changePercentOff() is defined in utils.js, called in index.html, ln 62
+changePercentOff( 32 );
--- utils.js
+++ utils.js
@@ -1,3 +1,10 @@
+// Whatever we pass to (message) displays in .textContent = message;
 function showMessage(message) {
     document.getElementById('message').textContent = message;
+}
+// 'document' refers to the whole webpage
+
+// 'percent-off' element is called in index.html, ln 62
+function changePercentOff(percentage) {
+    document.getElementById('percent-off').textContent = percentage + "% OFF";
 }
