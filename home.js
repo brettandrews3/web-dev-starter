26b5d11b44bde34161f8d32bcc85a2983be79988
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-03-08 15:57:48 -0600

    Lesson 7.5 - Function Return Values

--- home.js
+++ home.js
@@ -1,30 +1,10 @@
 // PluralSight JS 7: Functions
-// Function Expressions
-/*
-// Add message here. The block of code in {} will be stored in message.
-function showMessage(message) {
-    console.log(message);
+// Function Return Values
+
+function getSecretCode(value) {
+    //let code = value * 42;
+    //return code;
+    return value * 42
 }
 
-// Here, whatever we enter in () will be passed to console because
-// we're passing 'message' through to the console.log().
-showMessage("I've got information, man!");
-showMessage("What in god's name are you blathering about?");
-*/
-
-/*
-// We can also pass multiple types and values in the same function:
-function showMessages(message, anotherMessage) {
-    console.log(message, anotherMessage);
-}
-
-showMessages('She probably kidnapped herself.', "Obviously, you're not a golfer.");
-*/
-
-let myFunction = function (message, firstName) {
-    console.log(message);
-    console.log(firstName);
-}
-myFunction('Ever thus to deadbeats,', 'Lebowski.');
-// Any unsupplied parameters will be set to 'undefined'
-// Always make sure that you're passing correct values to functions
+console.log( getSecretCode(2) );    // returns 84
