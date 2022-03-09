e5cc6242c5fed11e06fc3659b57f250f56bfe90d
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-03-09 14:15:02 -0600

    Lesson 8.3 - Object Methods
    1st practice for defining a method within an object, then calling method outside the object.

--- home.js
+++ home.js
@@ -1,24 +1,15 @@
 // PluralSight JS 8: Objects and the DOM
-// Object Properties
+// Object Methods
 
-// Symbol() allows you to set properties in an object that only you can see:
-let mySymbol = Symbol();
+// Using our object from the last commit, we're adding a method to it:
 
-// Example of an object with multiple property types:
 let person = {
     name: 'John',
-    age: 34,
+    age: 42,
     partTime: false,
-    [mySymbol]: "It's a secret to everybody."
-    // [mySymbol] only accessible by code that has access to it elsewhere
+    showInfo: function(realAge) {
+        showMessage(this.name + ' is ' + realAge);
+    }
 };
 
-// Change property value within an object by assigning a new value:
-//person.age = 52;
-// ...or use square bracket notation to make the change:
-person['age'] = 16;
-
-console.log(person.name);       // John
-console.log(person.age);        // 52
-console.log(person.partTime);   // false
-showMessage(person.age);        // 16
+person.showInfo(34);    // 'John is 34' in showMessage
