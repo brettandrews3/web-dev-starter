13bd2f5029997fdc8353ec0ef3487f97bb564d9c
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-03-10 11:14:58 -0600

    Lesson 8.9 - Showing and Hiding DOM Elements

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 // PluralSight JS 8: Objects and the DOM
-// Detecting Button Clicks
+// Showing and Hiding DOM Elements
 
 /*
 In this lesson, we're reviewing the SEE REVIEW button from the website 
@@ -8,9 +8,41 @@ for Trail Review: Asphalt National Park. ~ln 124 on index.html
 Remove the href tag from the line. Add id="see-review" to make it
 editable by JavaScript.
 */
-
+/*
 const button = document.getElementById('see-review');
 
 button.addEventListener('click', function() {
     console.log('click');
 } );
+*/
+
+// Lesson 8.9 additions. See <div> in index.html, ~ln 130 for context.
+
+/*
+// Edits to button below will grab the element 'review', then remove the 
+// do-nothing 'd-none' class from the review element in index.html.
+const button = document.getElementById('see-review');
+
+button.addEventListener('click', function() {
+    const review = document.getElementById('review');
+
+    review.classList.remove('d-none');
+});
+*/
+
+// Toggle the review by clicking the button again. Add if statement to do this:
+const button = document.getElementById('see-review');
+
+button.addEventListener('click', function() {
+    const review = document.getElementById('review');
+
+    if (review.classList.contains('d-none')) {
+        review.classList.remove('d-none');
+        button.textContent = 'CLOSE REVIEW';
+    }
+    else {
+        review.classList.add('d-none');
+        button.textContent = 'SEE REVIEW';
+    }
+
+});
