40fe2ab5ede7d5b55a9a23e9fbbe039f07c8e213
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-03-10 10:21:49 -0600

    Lesson 8.6 - The Document Object Model DOM

--- utils.js
+++ utils.js
@@ -1,8 +1,16 @@
 // Whatever we pass to (message) displays in .textContent = message;
+/*
+Lesson 8.6: We're using the DOM on this page. Keyword 'document' refers
+to the web page.
+- DOM 'Element': a tag in the markup, e.g. <h1></h1>
+    - Access the element by its id: <h1 id="message"...</h1>
+    - Get the element by calling this method on utils.js:
+        function showMessage...
+            *document.getElementById('message').textContent = message;"
+*/
 function showMessage(message) {
     document.getElementById('message').textContent = message;
 }
-// 'document' refers to the whole webpage
 
 // 'percent-off' element is called in index.html, ln 62
 function changePercentOff(percentage) {
