ee032dd8159737a5814fa9769da3aab9eecb8207
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-03-10 10:30:12 -0600

    Lesson 8.7 - Styling DOM Elements

--- home.js
+++ home.js
@@ -1,27 +1,11 @@
 // PluralSight JS 8: Objects and the DOM
-// Standard Built-in Objects
+// Styling DOM Elements
 
-/*
-Info used in this lesson comes from Mozilla Developer Network
+// We're grabbing the 'GET A GRIP' element on index.html, around ln 60
+const header = document.getElementById('message');
 
-- The resource includes details about arrays: groups of objects | vaLues
-- Date(): includes methods like .getDate(), .getFullYear, .getUTCDate(), etc
-    - Instantiate this object by calling 'new Date();'
-    - A useful function: Date.toDateString() converts numerical dates to strings
-*/
+// To change its color, we can use the color name or its hexcode:
+header.style.color = 'green';
+header.style.color = '#FFF';
 
-/*
-// Get the current date and store it in a variable:
-let now = new Date();
-showMessage( now.toDateString() );    // Wed Mar 09 2022
-*/
-
-/*
-// Testing Math values and methods from MDN:
-showMessage( Math.abs(-42) );    // 42
-showMessage( Math.random() );    // 0.7119836274701075
-*/
-
-let s = 'I like your style, Dude.';
-showMessage( s.charAt(5) );     // e
-showMessage( Number.MIN_SAFE_INTEGER );    //-9007199254740991
+header.style.fontWeight = '800';
