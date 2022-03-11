34e9dbe1cd14538991f81e85decf2511433e400e
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-03-11 16:26:54 -0600

    Lesson 9.4 - Manipulating Arrays

--- home.js
+++ home.js
@@ -1,10 +1,46 @@
 // PluralSight JS 9: Arrays
-// Accessing Array Items
+// Manipulating Arrays
 
+/*
+// push() adds items to the end of an array:
 const values = [ 'a', 'b', 'c' ];
+values.push('d');
 
-values[0] = 'aaa';
-console.log( values[0] );    // aaa
-console.log( values[1] );    // b
-console.log( values[2] );    // c
-console.log( values[3] );    // undefined
+console.log( values );          // a b c d
+*/
+
+/*
+// pop() removes the last item in an array
+const values = [ 'a', 'b', 'c', 'd' ];
+const last = values.pop();
+console.log( last );     // d
+*/
+
+/*
+// shift() drops the first item in the array:
+const values = [ 'a', 'b', 'c', 'd' ];
+const first = values.shift();
+console.log( first );           // a
+console.log ( values );         // b c d
+*/
+
+/*
+// unshift() adds a new first item to the array:
+const values = [ 'b', 'c', 'd' ];
+values.unshift('a');
+console.log( values );          // a b c d
+*/
+
+// Putting it all together:
+const values = [ 'a', 'b', 'c' ];
+values.push( 'd', 'e', 'f' );
+console.log( values );          // a b c d e f
+
+const last = values.pop();
+console.log( values, last );    // [a b c d e] f
+
+const first = values.shift();
+console.log( values, first );   // [b c d e] a
+
+values.unshift('hello', 'world');
+console.log( values );          // [hello world b c d e]
